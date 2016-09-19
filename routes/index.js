c00794f1ec858eb1ac7a5a0bfdbf0382f32fb588
ScytaleTleilax <andreivradulescu@gmail.com>
2016-09-19 17:13:21 +0300

    test

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,24 @@
-var express = require('express');
+"use strict";
+var express = require( 'express' );
 var router = express.Router();
 
-const MongoCRUD = require("./../mongo/MongoCRUD");
-var app = require("./../app");
+const MongoCRUD = require( "./../mongo/MongoCRUD" );
+var app = require( "./../app" );
 
 /* GET home page. */
 
 
-router.get('/', function (req, res) {
+router.get( '/' , function ( req , res ) {
 
-    var CRUD = new MongoCRUD("contacts");
+    var CRUD = new MongoCRUD( "contacts" );
 
-    CRUD.getAll(function (result) {
-        res.json(result);
-    });
+    CRUD.getAll( function ( result ) {
 
-    /*
-     res.render('index', {
-     title: 'Express'
-     });*/
-});
+        res.render( 'index' , {
+            title : 'MEAN shop' ,
+            docs : result
+        } )
+    } );
+} );
 
 module.exports = router;
